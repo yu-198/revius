ae17f0345e55000e0316cf1252e03a4339877d14
yu-198 <fevrier39seize@gmail.com>
2020-01-16 06:13:00 +0000

    Fix index_action create_action enable_action disable_action  Add strong parameter

--- app/controllers/admins/genres_controller.rb
+++ app/controllers/admins/genres_controller.rb
@@ -1,11 +1,19 @@
 class Admins::GenresController < ApplicationController
   def index
+    @genre = Genre.new
+    @genres = Genre.all
   end
 
   def edit
   end
 
   def create
+    @genre = Genre.new(genre_params)
+    if @genre.save
+       redirect_to admins_genres_path
+    else
+       render :index
+    end
   end
 
   def update
@@ -24,5 +32,10 @@ class Admins::GenresController < ApplicationController
   	@genre.save
   	redirect_to admins_genres_path
   end
+
+  private
+    def genre_params
+      params.require(:genre).permit(:name, :id_disable)
+    end
 
 end
