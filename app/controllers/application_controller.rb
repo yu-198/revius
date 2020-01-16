2ec9f2777e18e6fd0f76e181e6d7b535a97a8058
yu-198 <fevrier39seize@gmail.com>
2020-01-16 06:15:02 +0000

    Fix after_sign_in_path after_sign_out_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::Base
+	protected
+		def configure_permitted_parameters
+			devise_parameter_sanitizer.permit(:sign_up,keys:[:last_name, :first_name, :last_name_kana, :frist_name_kana, :postal_code, :address, :phone])
+		end
+	private
+		def after_sign_in_path_for(resource)
+			case resource
+				when Admin
+				admins_path
+				when User
+				root_path
+			end
+		end
+		def after_sign_out_path_for(resource)
+			case resource
+				when :admin
+				new_admin_session_path
+				when :user
+				root_path
+			end
+		end
 end
