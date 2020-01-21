9fcaeeb014b22d3fe2572ed9a152b332f8d4afd4
yu-198 <fevrier39seize@gmail.com>
2020-01-21 07:17:02 +0000

    Add Bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,12 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require activestorage
 //= require turbolinks
+//= require jquery
+//= require bootstrap-sprockets
 //= require_tree .
+//= require activestorage
+
+
+
+
