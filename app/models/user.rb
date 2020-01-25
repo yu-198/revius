d05dd274ec2d91eec124f9d200e578bd43105750
yu-198 <fevrier39seize@gmail.com>
2020-01-25 12:48:35 +0000

    Add validation and Fix layouts

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :orders, dependent: :destroy
 
+  VALID_EMAIL_REGEX = /\A\d{10,11}\z/
+  validates :phone,presence: true, format: {with: VALID_EMAIL_REGEX}
+
 end
