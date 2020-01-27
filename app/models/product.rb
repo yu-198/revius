212d5785274335e1fa624799cc7b6ceb99d7dc35
yu-198 <fevrier39seize@gmail.com>
2020-01-27 10:12:50 +0000

    Add validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,8 @@ class Product < ApplicationRecord
 	# belongs_to :article
 	attachment :image
 
+	validates :introduction, length: { maximum: 100 }
+
 	# 擬似的にorderに擬似的にカラムを作っている(product_priceの値を税有りの金額で入れるため)
 	def tax_include_price
 		(price * 1.1).round
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,7 @@ class User < ApplicationRecord
 
   VALID_EMAIL_REGEX = /\A\d{10,11}\z/
   validates :phone,presence: true, format: {with: VALID_EMAIL_REGEX}
-
+  validates :last_name, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
+  validates :first_name, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
+  validates :postal_code, format: /\A[0-9]+\z/
 end
